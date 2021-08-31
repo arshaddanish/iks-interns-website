7629027891534024d497c7ba0c77147431d30097
Arshad Danish <arshaddanishthana@gmail.com>
2021-08-31 22:39:32 +0530

    update portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,9 +199,9 @@
   /**
    * Initiate portfolio lightbox
    */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
-  });
+  // const portfolioLightbox = GLightbox({
+  //   selector: ".portfolio-lightbox",
+  // });
 
   /**
    * Portfolio details slider

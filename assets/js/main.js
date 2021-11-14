a0c8aa8c2e90ec20a047ead13d198f2f433d3508
Arshad Danish <arshaddanishthana@gmail.com>
2021-11-14 18:48:08 +0530

    minor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+window.addEventListener("load", () => {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+    mirror: false,
+  });
+});
+
 (function () {
   "use strict";
   /**

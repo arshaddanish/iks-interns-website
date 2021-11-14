3bf80d0d27f589f11f2db4ccb0846b64f8376195
Arshad Danish <arshaddanishthana@gmail.com>
2021-11-14 18:43:31 +0530

    minor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,5 @@
 (function () {
   "use strict";
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
-  });
-
   /**
    * Easy selector helper function
    */

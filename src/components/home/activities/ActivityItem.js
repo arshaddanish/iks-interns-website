72815f88722790b9b7b5bac7d23ffebf7a8f393e
Arshad Danish <arshaddanishthana@gmail.com>
2022-01-25 19:46:48 +0530

    debug

--- src/components/home/activities/ActivityItem.js
+++ src/components/home/activities/ActivityItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ActivityItem({ icon, title, content }) {
   return (
@@ -11,7 +12,9 @@ export default function ActivityItem({ icon, title, content }) {
           <i class={icon}></i>
         </div>
         <h4 class="title">
-          <a class="inactiveLink">{title}</a>
+          <Link to="" class="inactiveLink" onClick={(e) => e.preventDefault()}>
+            {title}
+          </Link>
         </h4>
         <p class="description">{content}</p>
       </div>

68f3cf8b438030990575ec94cdefb80a69857314
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2022-02-19 01:18:30 +0530

    color and font optimizations

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,11 +1,11 @@
-const sections = document.getElementsByClassName('team-profile');
-for(let i=0; i<sections.length; i++) {
-    let profiles = sections[i].children;
-    //console.log(profiles)
-    for(let j=0; j<profiles.length; j++) {
-        let profile = profiles[j];
-        profile.addEventListener('click', () => {
-            location.href = profile.children[3].href;
-        })
-    }
-}
+// const sections = document.getElementsByClassName('team-profile');
+// for(let i=0; i<sections.length; i++) {
+//     let profiles = sections[i].children;
+
+//     for(let j=0; j<profiles.length; j++) {
+//         let profile = profiles[j];
+//         profile.addEventListener('click', () => {
+//             window.open(profile.children[3].href, '_blank');
+//         })
+//     }
+// }
